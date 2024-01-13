b9d259ef70cb32b39b9d82fb410dfbf8d8146502
unknown <vikram.peddinti@gmail.com>
2024-01-12 20:18:37 -0800

    Added working navbar with basic Links, using the map method

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,6 +4,31 @@
 
 import * as React from 'react';
 import SLink from './SLink';
+import { Link } from 'gatsby';
+
+// Inspired by https://github.com/64bitpandas/bencuan.me/blob/main/src/components/header.js
+// Used the little hack: (i + 1 !== row.length)
+// Store a name which is displayed on the navbar along with the url to link to
+const barLinks = [
+  {
+    name: "home",
+    url: "/"
+  },
+  {
+    name: "about",
+    url: "/about"
+  },
+  {
+    name: "projects",
+    url: "/projects"
+  },
+  {
+    name: "contact",
+    url: "/contact"
+  }
+]
+
+
 
 // main page component that user lands on
 const Navbar = () => {
@@ -14,8 +39,17 @@ const Navbar = () => {
       <nav className='bg-stone-100 py-5'>
         <div className='flex justify-center items-center'>
           {/* Add each item in the flex container */}
+
           <div className='flex-none font-semibold text-lg md:text-xl px-6'>
-            <SLink>home</SLink> | <SLink>about</SLink> | <SLink>projects</SLink> | <SLink>contact</SLink></div>
+          {
+          barLinks.map((link, i, row) => (
+                  <span key={link.name}>
+                  <Link className='hover:underline' to={link.url}>{link.name}</Link>
+                  {(i + 1 !== row.length) && (<span>&nbsp;|&nbsp;</span>)}
+                </span>
+          ))
+          }
+          </div>
         </div>
       </nav>
 
